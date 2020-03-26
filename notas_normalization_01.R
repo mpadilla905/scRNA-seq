9fb6f8bdf0a5d30c7f7a407273bef242cdbee691
mpadilla905 <54638497+mpadilla905@users.noreply.github.com>
2020-03-26 14:25:41 -0600

    Update notas_normalization_01.R
    added notes and some answers

--- notas_normalization_01.R
+++ notas_normalization_01.R
@@ -2,24 +2,46 @@
 library('scRNAseq')
 sce.zeisel <- ZeiselBrainData(ensembl = TRUE)
 
+
 # Quality control
 library('scater')
 is.mito <- which(rowData(sce.zeisel)$featureType == "mito")
+## cuales genes son mitocondrial
 stats <- perCellQCMetrics(sce.zeisel, subsets = list(Mt = is.mito))
 qc <-
     quickPerCellQC(stats,
         percent_subsets = c("altexps_ERCC_percent", "subsets_Mt_percent"))
+## QUCKKper cell corre de fondo isOutlier
+
 sce.zeisel <- sce.zeisel[, !qc$discard]
+## descartamos las celulas de baja calidad 190 de 3000
+table(qc$discard)
+
 
 
 ## ----all_code2, cache=TRUE, dependson='all_code'---------------------------------------------------------------------
 # Library size factors
 lib.sf.zeisel <- librarySizeFactors(sce.zeisel)
+## esta funcion nos va a dar el size factor dependiendo de la libreria
+## librery size = sum of counts across all genes for each cell
+
+class(lib.sf.zeisel)
+length(lib.sf.zeisel)
+mean(lib.sf.zeisel)
+head(lib.sf.zeisel)
 
 # Examine distribution of size factors
 summary(lib.sf.zeisel)
 hist(log10(lib.sf.zeisel), xlab = "Log10[Size factor]", col = "grey80")
+
+head(counts(sce.zeisel))
+
+## contar manualmente el library size
 ls.zeisel <- colSums(counts(sce.zeisel))
+head(ls.zeisel)
+
+identical(lib.sf.zeisel,ls.zeisel)
+
 plot(
     ls.zeisel,
     lib.sf.zeisel,
@@ -28,6 +50,39 @@ plot(
     ylab = "Size factor"
 )
 
+##########################################################################
+## EXERCISE 
+
+## Are ls.zeisel and lib.sf.zeisel identical?
+m = match(names(lib.sf.zeisel),names(ls.zeisel))
+ls.zeisel = ls.zeisel[m]
+identical(lib.sf.zeisel,ls.zeisel)
+## FALSE
+
+## Are they proportional? yes
+
+length(lib.sf.zeisel) ## 2815
+length(ls.zeisel) ## 2815
+
+sum(lib.sf.zeisel/ls.zeisel)/length(lib.sf.zeisel) # 6.686916e-05
+x = lib.sf.zeisel/ls.zeisel
+## checar si en x todo es igual
+## plot(lib.sf.zeisel, ls.zeisel) ## ninguno se sale de la diagonal perf
+
+# otra opcion
+table(lib.sf.zeisel/ls.zeisel)
+## 6.68691586731496e-05 
+##                2815 
+
+## Compute lib.sf.zeisel manually
+
+## Library size = sum of counts across all genes for each cell
+## The "library size factor" for each cell is then scaled so that 
+##       the average size factor across all cells is 1
+
+
+
+##########################################################################
 
 ## ----exercise_solution, cache=TRUE, dependson='all_code'-------------------------------------------------------------
 ## First compute the sums
